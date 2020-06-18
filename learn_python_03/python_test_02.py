5a4ea4e8086f4d4871c209a25cd98acb92113556
0xMilk <1144502582@qq.com>
2020-06-18 21:10:20 +0800

    用户输入输出以及部分类型转换函数

---
+++ learn_python_03/python_test_02.py
@@ -0,0 +1,16 @@
+# 与用户交互
+# 输入语句
+# 在python3中，input方法会将所有输入的内容转换成字符串
+# username = input("请输入用户名: ")
+# print(username, type(username))
+
+# 类型转换
+# print("请输入年龄: ")
+# age = input()
+# age = int(age)  # int函数只能将纯数字的字符串转成整型，不包括小数的字符串形式
+# print(age, type(age))
+# x = float("12.5")
+# print(x, type(x))
+# l = list("[1,2,3]")
+# y = type(l)
+# print(l, type(y))
