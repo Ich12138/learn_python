b54919b1c059a40a9af8da19fe02d2fff54194e3
0xMilk <1144502582@qq.com>
2020-06-18 20:53:22 +0800

    数字，字符串，列表和字典

---
+++ learn_python_02/python_test_02.py
@@ -0,0 +1,70 @@
+# 1 数字类型
+# 1.1 整型int
+# 定义
+# age = 18
+# print(type(age))
+
+# 1.2浮点型float
+# 定义
+# salary = 1314.78
+# print(type(salary))
+
+# 数字类型的其他使用
+# level = 1
+# x = level + 1
+
+
+# 2 字符串类型str
+# 定义：
+# name = 'wangqain'
+# print(name)
+
+# 其他使用
+# 注意字符串的嵌套: 外层使用单引号，内层使用双引号
+# print("我是 '王骞' ")
+# print('我是 "王骞" ')
+# print('我是 \'王骞\' ')
+
+# 字符串之间可以相加，但仅限于字符串与字符串之间相加
+# 代表字符串的拼接，但效率很低，了解
+# print('aaa' + 'cccc')
+# print('aaa' * 4)
+
+
+# 3 列表：索引对应值
+# 作用: 记录多个值，并且可以按照索引取
+# 定义：在[]内用都好分隔开多个任意类型的元素
+# l = [10, 3.1, 'ccc', ["ccc", "vvvv"], 'ccc']
+# print(l)
+# 可以正着取
+# print(l[3][1])
+
+# 还可以倒着取
+# print(l[-2])
+
+# 其他的用途
+# student_info = [
+#     ['tony', 18, ['read', 'sing']],
+#     ['peter', 20, ['sleep', 'run']]
+# ]
+
+# print(student_info[0][2])
+
+# 字典类型：key对应值，其中key通常为字符串类型，所以key
+# 对值可以有描述性的共
+# 作用：用来存多个值，每个值都有唯一一个key与其对应，key对值有描述性功能
+# 定义：在{}内用多个逗号分开多个key:value
+# k = {'a': 1, 'b': 2}
+# print(type(k))  # 展示类型
+# print(k['a'])  # 取值
+# print(k)
+
+
+
+
+
+
+# 布尔类型
+# 定义：
+a = True
+b = False
