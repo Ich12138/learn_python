4b5a0dc1e26b3961663f46b279263197c4c96454
0xMilk <1144502582@qq.com>
2020-06-28 14:40:07 +0800

    if语法1

---
+++ learn_python_04/python_test_04.py
@@ -0,0 +1,18 @@
+# if判断语法
+# 语法1：
+# if 条件：
+#     代码一
+#     代码三
+#     。。。。
+
+
+age = int(input("请输入年龄: "))
+is_beautiful = True
+star = "水瓶座"
+
+if 16 < age < 20 and is_beautiful and star == "水瓶座":
+    print("我喜欢你")
+else:
+    print("咱俩不合适")
+
+print("其他代码.....")
