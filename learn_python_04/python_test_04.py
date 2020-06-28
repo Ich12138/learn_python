e64b5dedb22f00116cc8dae7c2b97683c785739d
0xMilk <1144502582@qq.com>
2020-06-28 14:43:13 +0800

    if语法2

--- learn_python_04/python_test_04.py
+++ learn_python_04/python_test_04.py
@@ -6,13 +6,24 @@
 #     。。。。
 
 
-age = int(input("请输入年龄: "))
-is_beautiful = True
-star = "水瓶座"
+# age = int(input("请输入年龄: "))
+# is_beautiful = True
+# star = "水瓶座"
+#
+# if 16 < age < 20 and is_beautiful and star == "水瓶座":
+#     print("我喜欢你")
+# else:
+#     print("咱俩不合适")
+#
+# print("其他代码.....")
 
-if 16 < age < 20 and is_beautiful and star == "水瓶座":
-    print("我喜欢你")
-else:
-    print("咱俩不合适")
 
-print("其他代码.....")
+# if语法二:
+# if 条件:
+#     代码1
+#     代码2
+#     ....
+# else :
+#     代码1
+#     代码2
+#     ....
