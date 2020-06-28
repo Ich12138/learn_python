4cf46bab84bf7c6eef573b0203e066b61643478a
0xMilk <1144502582@qq.com>
2020-06-28 10:56:43 +0800

    逻辑运算符

--- learn_python_04/python_test_02.py
+++ learn_python_04/python_test_02.py
@@ -7,5 +7,16 @@
 # 其中0，None,空(空字符串，空列表，空字典)=》代表布尔值为False，其余类型为真
 
 
+# 3、逻辑运算符
+# not:就是把紧跟其后的那个条件结果取反
+# ps:not与紧跟其后的那个条件是一个不可分割的整体
+# print(not 16 > 13)
+# print(not 10)
+# print(not 0)
+# print(not None)
+# print(not "")
 
+
+# and:逻辑与，用来连接左右两个条件，同真为真，一假则假
+print(16 > 1 and True)
 
