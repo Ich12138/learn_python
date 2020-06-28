1189310381c2af5104b0642bf9c75ee5cfd569e0
0xMilk <1144502582@qq.com>
2020-06-28 14:25:29 +0800

    成员运算符

---
+++ learn_python_04/python_test_03.py
@@ -0,0 +1,14 @@
+# 成员运算符 in
+# 判断B字符串是否包含字串A
+# res = "wq" in "hello wq"
+# print(res)
+
+# 判断元素是否在列表里面
+# print(111 in [111, 222, 333])
+
+# 判断key是否在字典中,不能判断value
+# print("k1" in {"k1": 111, "k2": 222})
+
+# not in
+# res = "wq" not in "hello wq"
+# print(res)
