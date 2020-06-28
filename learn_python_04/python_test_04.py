05d36f7b620aef25389627ea2382d9348b1953e3
0xMilk <1144502582@qq.com>
2020-06-28 14:55:16 +0800

    if语法3

--- learn_python_04/python_test_04.py
+++ learn_python_04/python_test_04.py
@@ -27,3 +27,25 @@
 #     代码1
 #     代码2
 #     ....
+
+
+# if语法三:
+# if 条件:
+#     代码1
+#     代码2
+#     ....
+# elif 条件:
+#     代码1
+#     代码2
+#     ....
+
+
+# score = int(input("请输入你的成绩: "))
+# if score < 60:
+#     print("不及格")
+# elif 60 <= score <= 70:
+#     print("合格")
+# elif 70 < score <= 80:
+#     print("良")
+# else:
+#     print("优秀")
