82a0b6144cf78876d293213d65d5f9d858af68b9
0xMilk <1144502582@qq.com>
2020-06-29 18:29:41 +0800

    while死循环

--- learn_python_05/python_test_02.py
+++ learn_python_05/python_test_02.py
@@ -6,7 +6,14 @@ while 条件:
       ....
 """
 
+# count = 0
+# while count < 5:
+#     print(count)
+#     count += 1
+
+# 2、死循环与效率问题
+# 纯计算无io的死循环会导致致命的效率问题
+
 count = 0
 while count < 5:
     print(count)
-    count += 1
