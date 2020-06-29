165eacfd0928a5ceabc3539b2e3cd7036e48179d
0xMilk <1144502582@qq.com>
2020-06-29 19:10:52 +0800

    while循环嵌套

--- learn_python_05/python_test_02.py
+++ learn_python_05/python_test_02.py
@@ -20,18 +20,18 @@ while 条件:
 
 # 3、循环的应用
 
-user_name = "wq"
-user_pwd = "123"
-
-while True:
-    input_name = input("请输入用户名: ")
-    input_pwd = input("请输入密码: ")
-    if input_name == user_name and input_pwd == user_pwd:
-        print("登陆成功")
-        exit()
-        # break
-    else:
-        print("账户或者密码错误!")
+# user_name = "wq"
+# user_pwd = "123"
+#
+# while True:
+#     input_name = input("请输入用户名: ")
+#     input_pwd = input("请输入密码: ")
+#     if input_name == user_name and input_pwd == user_pwd:
+#         print("登陆成功")
+#         exit()
+#         # break
+#     else:
+#         print("账户或者密码错误!")
 
 
 # 4、退出循环的两种方式
@@ -40,4 +40,21 @@ while True:
 
 
 # 7、while循环的嵌套
+user_name = "wq"
+user_pwd = "123"
+
+while True:
+    input_name = input("请输入用户名: ")
+    input_pwd = input("请输入密码: ")
+    if input_name == user_name and input_pwd == user_pwd:
+        print("登陆成功")
+        while True:
+            cmd = input("请输入指令: ")
+            if cmd == "q":
+                break
+            print("命令{cmd}正在执行".format(cmd=cmd))
+        break
+    else:
+        print("账户或者密码错误!")
+
 
