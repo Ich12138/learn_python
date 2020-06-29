4d41791006740e222a4d67c118a78ee8ef70a7e7
0xMilk <1144502582@qq.com>
2020-06-29 20:51:50 +0800

    输出1-100所有奇数

--- learn_python_05/python_test_03.py
+++ learn_python_05/python_test_03.py
@@ -10,9 +10,17 @@
 #
 
 # 2、求 1-100所有数的和
-count2 = 1
-sum1 = 0
-while count2 <= 100:
-    sum1 += count2
-    count2 += 1
-print(sum1)
+# count2 = 1
+# sum1 = 0
+# while count2 <= 100:
+#     sum1 += count2
+#     count2 += 1
+# print(sum1)
+
+
+# 3、输出1-100所有奇数
+# count3 = 1
+# while count3 <= 100:
+#     if count3 % 2 != 0:
+#         print(count3)
+#     count3 += 1
