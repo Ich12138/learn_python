fd6053f47a483e7a8b2ba22e5cdb527be251b9f6
0xMilk <1144502582@qq.com>
2020-06-29 20:48:20 +0800

    求 1-100所有数的和

--- learn_python_05/python_test_03.py
+++ learn_python_05/python_test_03.py
@@ -1,10 +1,18 @@
 # 简单练习
 # 1、使用while打印1 2 3 4 5 6   8 9 10
-count1 = 1
-while count1 <= 10:
-    if count1 == 7:
-        count1 += 1
-        continue
-    print(count1)
-    count1 += 1
-    
+# count1 = 1
+# while count1 <= 10:
+#     if count1 == 7:
+#         count1 += 1
+#         continue
+#     print(count1)
+#     count1 += 1
+#
+
+# 2、求 1-100所有数的和
+count2 = 1
+sum1 = 0
+while count2 <= 100:
+    sum1 += count2
+    count2 += 1
+print(sum1)
