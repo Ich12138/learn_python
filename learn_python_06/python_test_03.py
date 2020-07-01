39b98234f9dd14e3b3a763ed3ffb09817bd36ba7
0xMilk <1144502582@qq.com>
2020-07-01 18:46:20 +0800

    打印金字塔

--- learn_python_06/python_test_03.py
+++ learn_python_06/python_test_03.py
@@ -5,3 +5,15 @@
 #         print("{i} * {j} = {res}".format(i=i, j=j, res=i * j), end="      ")
 #     print()
 
+
+# 2、打印金字塔
+# 数学表达式
+# 空格数 = max_level - current_level
+# *号数 = 2 * current_level - 1
+max_level = 5
+for current_level in range(max_level):
+    for k in range(max_level - current_level):
+        print(" ", end="")
+    for j in range(2 * current_level - 1):
+        print("*", end="")
+    print()
