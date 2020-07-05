59af523e8ed623e1fa51880ee02feb261bbd3402
0xMilk <1144502582@qq.com>
2020-07-05 19:34:30 +0800

    字典的创建

--- learn_python_07/python_test_03.py
+++ learn_python_07/python_test_03.py
@@ -6,5 +6,43 @@
 
 # d = dict()
 # print(d, type(d))
-d = dict(x=1, y=2, z="qwe")
-print(d)
+# d = dict(x=1, y=2, z="qwe")
+# print(d)
+
+
+# 数据类型转换
+# info = [
+#     ["name", "wq"],
+#     ("age", 18),
+#     ["gender", "male"]
+# ]
+
+# d = {}
+
+# 方式一
+# for item in info:
+#     d[item[0]] = item[1]
+# print(d)
+
+# 方式二  利用解压操作
+# for k, v in info:
+#     d[k] = v
+# print(d)
+
+
+# res = dict(info)
+# print(res)
+
+
+# 创造一个value为None的字典
+# 方式一
+keys = ["name", "age", "gender"]
+# values = None
+# d = {}
+# for item in keys:
+#     d[item] = values
+# print(d)
+
+
+# 方式二
+print({}.fromkeys(keys))
