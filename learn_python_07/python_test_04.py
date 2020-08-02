085899c0d1318360a1103f7d5da1c3ca208149ac
Wq <1144502582@qq.com>
2020-08-02 17:47:50 +0800

    集合运算关系

--- learn_python_07/python_test_04.py
+++ learn_python_07/python_test_04.py
@@ -13,7 +13,6 @@
 # 1.2去重
 
 
-
 # 定义：在{}内用逗号分隔开多个元素。，集和具备以下三个特点:
 # 1: 每个元素必须是不可变类型
 # 2: 集合内没有重复元素
@@ -32,12 +31,46 @@
 # print(s, type(s))  # {1, 2, 3, 4} <class 'set'>
 
 # 类型转换
-res = set("helloqwedfrt")
-print(res)
+# res = set("helloqwedfrt")
+# print(res)
 
 # 但凡能被for循环的遍历的数据类型（强调：遍历出的每一个值都必须为不可变类型）都可以传给set()转换成集合类型
 # s = set([1, 2, 3, 4])
 # s = set((1, 2, 3, 4))
 # print(s)
 
+# 内置方法
+friends1 = {"zero", "kevin", "jason", "egon"}  # 用户1的好友们
+friends2 = {"Jy", "ricky", "jason", "egon"}  # 用户2的好友们
 
+# 1：取交集：两者共同好友
+# res = friends1 & friends2
+# print(res)
+
+# 2：取并集，两个所有好友
+# res = friends1 | friends2
+# print(res)
+
+# 3：差集(有左右顺序之分)，去friends1独有的好友
+# res = friends1 - friends2
+# print(res)
+
+# 4：对称差集(^) # 求两个用户独有的好友们（即去掉共有的好友）
+# res = friends1 ^ friends2
+# print(res)
+
+# 5：父子集：包含关系
+# s1 = {1, 2, 3}
+# s2 = {1, 2, 4}
+# 不存在包含关系，下面比较均为false
+# print(s1 > s2)
+# print(s1 < s2)
+
+# s1 = {1, 2, 3}
+# s2 = {1, 2}
+# print(s1 > s2)  # s1是s2的父
+
+
+# s1 = {1, 2, 3}
+# s2 = {1, 2, 3}
+# print(s1 == s2)  # s1,s2互为父子
