267fc28fb9e37eb1e3a45fc7dcc6033b3edcde24
Wq <1144502582@qq.com>
2020-09-13 17:51:38 +0800

    操作文件基本流程

--- learn_python_08/python_test_01.py
+++ learn_python_08/python_test_01.py
@@ -1,1 +1,13 @@
 # 文件
+# 文件操作基本流程
+# 1、打开文件
+# 若不指定绝对路径,则会从当前py文件的路径下开始找
+# 文件的相对位置，打开文件的模式，打开文件的编码
+file = open('../doc/test.txt', mode='rt', encoding="utf-8")
+print(file)
+# 2、操作文件: 读或者写
+res = file.read()
+print(res)
+# 3、关闭文件
+file.close()
+# del file  # 可不需要
