60d62473bf31eb5d0551861042ee0aa6b4be002a
Wq <1144502582@qq.com>
2020-09-23 09:35:48 +0800

    控制文件指针操作补充

--- learn_python_08/python_file_07.py
+++ learn_python_08/python_file_07.py
@@ -15,3 +15,13 @@
 #       0: 文件指针从文件头开始
 #       1: 文件指针从当前位置开始
 #       2: 文件指针从文件末尾开始，应该让指针倒着移动
+# 强调: 只有0模式可以在t模式下使用,1、2只能在b模式下使用
+
+# with open(r'../doc/test.txt', mode='rb') as f2:
+#     print(f2.tell())
+#     f2.seek(100, 0)
+#     res = f2.read()
+#     print(res.decode('utf-8'))
+
+# 案例
+# 模拟linux里的 tail -f 命令
