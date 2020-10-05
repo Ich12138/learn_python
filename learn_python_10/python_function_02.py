7619d7e5b73dd01116a461a8198d7538e1dfe9c0
Wq <1144502582@qq.com>
2020-10-05 21:47:44 +0800

    函数对象应用实例-ATM例子

--- learn_python_10/python_function_02.py
+++ learn_python_10/python_function_02.py
@@ -1,1 +1,41 @@
 # 函数对象应用实例
+# 结合ATM取款机
+def login():
+    print("登录")
+
+
+def transfer():
+    print("转账功能")
+
+
+def check_balance():
+    print("查询余额")
+
+
+def withdraw():
+    print("提现")
+
+
+fun_dic = {
+    '1': ['登录', login],
+    '2': ['转账', transfer],
+    '3': ['查询余额', check_balance],
+    '4': ['提现', withdraw]
+}
+
+while True:
+    for k in fun_dic:
+        print(k, fun_dic[k][0])
+
+    command = input("请输入命令: ")
+    if not command.isdigit():
+        print("必须是以上编号,憨批")
+        continue
+
+    if command in fun_dic:
+        if fun_dic[command][0] is None:
+            break
+        else:
+            fun_dic[command][1]()
+    else:
+        print("没有该命令")
