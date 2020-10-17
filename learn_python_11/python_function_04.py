c75cd073787ab235a18f4b6a053d9e5d41ee7cac
Wq <1144502582@qq.com>
2020-10-17 18:58:44 +0800

    闭包函数简单应用

--- learn_python_11/python_function_04.py
+++ learn_python_11/python_function_04.py
@@ -3,7 +3,6 @@
 核心点：名字的查找关系是以函数定义阶段为准
 """
 
-
 # 闭包函数
 # "闭"函数指的是该函数是内嵌函数
 # "包"函数指的是该函数包含对外层函数作用域名字的引用(不是全局作用域)
@@ -20,20 +19,19 @@
 
 # 闭包函数: 函数对象
 # 无论在哪里调用f2函数, f2内所需要的变量值在定义阶段就已经确定
-def f1():
-    x = 1
+# def f1():
+#     x = 1
+#
+#     # 闭的提现
+#     def f2():
+#         print(x)  # 包的提现
+#
+#     return f2
 
-    # 闭的提现
-    def f2():
-        print(x)  # 包的提现
 
-    return f2
-
-
-f = f1()
-print(f)  # <function f1.<locals>.f2 at 0x01A387C0>
-f()
-
+# f = f1()
+# print(f)  # <function f1.<locals>.f2 at 0x01A387C0>
+# f()
 
 # 为何使用闭包函数 应用场景
 # 两种为函数体传参的方式
@@ -44,8 +42,25 @@ f()
 
 
 # 方式2:
-def f1():
-    x = 3
+# def f1():
+#     x = 3
+#
+#     def f2():
+#         print(x)
 
-    def f2():
-        print(x)
+
+import requests
+
+
+def outter(url):
+    def get():
+        response = requests.get(url)
+        print(len(response.text))
+
+    return get
+
+
+f = outter("https://www.baidu.com")
+f()
+# get("https://www.baidu.com")
+# get("http://47.107.64.157:8090")
