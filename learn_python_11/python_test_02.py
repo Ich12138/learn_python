b9536da4689b6109b4d8876f52136f3aeb036ddf
Wq <1144502582@qq.com>
2020-10-20 14:52:42 +0800

    编写装饰器, 为函数加上认证功能

--- learn_python_11/python_test_02.py
+++ learn_python_11/python_test_02.py
@@ -8,25 +8,61 @@ import time
 
 # 2、编写装饰器, 为函数加上统计时间功能
 
+# def time_counter(func):
+#     def wrapper(*args, **kwargs):
+#         start = time.time()
+#         res = func(*args, **kwargs)
+#         end = time.time()
+#         print("当前函数 {} 执行时间为: {} s".format(func.__name__, end - start))
+#         return res
+#
+#     return wrapper
+#
+#
+# @time_counter
+# def index(name):
+#     time.sleep(2)
+#     print("欢迎 {} 来到主页".format(name))
+#
+#
+# index("wq")
 
-def time_counter(func):
+# 3、编写装饰器, 为函数加上认证功能
+def login():
+    user_name = input("请输入账户:  ")
+    user_pwd = input("请输入密码:  ")
+    with open('./doc/account.txt', mode='rt', encoding="utf-8") as account_file:
+        msg = account_file.readline()
+        userinfo = eval(msg)
+    if userinfo['user'] == user_name:
+        if userinfo['password'] == user_pwd:
+            return True
+        else:
+            print("密码错误")
+            return False
+    else:
+        print("账户错误")
+        return False
+
+
+def auth(func):
     def wrapper(*args, **kwargs):
-        start = time.time()
-        res = func(*args, **kwargs)
-        end = time.time()
-        print("当前函数 {} 执行时间为: {} s".format(func.__name__, end - start))
-        return res
+        res = login()
+        if res:
+            print("登录成功!")
+            func(*args, **kwargs)
+        else:
+            print("登陆失败, 请重新登录!")
 
     return wrapper
 
 
-@time_counter
+@auth
 def index(name):
-    time.sleep(2)
     print("欢迎 {} 来到主页".format(name))
 
 
 index("wq")
 
-# 3、编写装饰器, 为函数加上认证功能
+
 # 4、编写装饰器, 为多个函数加上认证功能, 要求只需登录成功一次, 后续的函数都无需再输入用户名和密码
