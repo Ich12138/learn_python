5a7e59714da6de2c2152193e82679afb7ab68192
Wq <1144502582@qq.com>
2020-10-20 14:37:00 +0800

    编写装饰器, 为函数加上统计时间功能

--- learn_python_11/python_test_02.py
+++ learn_python_11/python_test_02.py
@@ -1,7 +1,32 @@
+import time
+
+
 # 1、编写一个函数
-def index():
-    print("欢迎来到主页")
-    
+# def index(name):
+#     print("欢迎 {} 来到主页".format(name))
+
+
 # 2、编写装饰器, 为函数加上统计时间功能
+
+
+def time_counter(func):
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        res = func(*args, **kwargs)
+        end = time.time()
+        print("当前函数 {} 执行时间为: {} s".format(func.__name__, end - start))
+        return res
+
+    return wrapper
+
+
+@time_counter
+def index(name):
+    time.sleep(2)
+    print("欢迎 {} 来到主页".format(name))
+
+
+index("wq")
+
 # 3、编写装饰器, 为函数加上认证功能
 # 4、编写装饰器, 为多个函数加上认证功能, 要求只需登录成功一次, 后续的函数都无需再输入用户名和密码
