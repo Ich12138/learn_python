2675ae53f71d17fbac8e50b29ffaabea4a675a27
Wq <1144502582@qq.com>
2020-10-24 14:47:11 +0800

    有参装饰器学习以及模板

--- learn_python_11/python_decorator_03.py
+++ learn_python_11/python_decorator_03.py
@@ -2,20 +2,80 @@
 有参装饰器
 """
 
+
 # 一: 知识储备
 # 由于语法糖的限制, outer函数只能有一个参数
 # 而且该参数只用来接收被装饰对象的内存地址
-def outer(func):
-    def wrapper(*args, **kwargs):
-        res = func(*args, **kwargs)
-        return res
+# def outer(func):
+#     def wrapper(*args, **kwargs):
+#         res = func(*args, **kwargs)
+#         return res
+#     return wrapper
+#
+#
+# @outer
+# def index(name):
+#     print("欢迎 {} ".format(name))
+#
+#
+# index("wq")
 
-    return wrapper
+# 有参装饰器
+def auth(db_type):
+    def deco(func):
+        def wrapper(*args, **kwargs):
+            name = input("your name: ")
+            password = input("your password: ")
+            if db_type == 'file':
+                print("来自文件验证")
+                if name == 'wq' and password == '123':
+                    print("login successful")
+                    res = func(*args, **kwargs)
+                    return res
+                else:
+                    print("user or pass error")
+            if db_type == 'mysql':
+                print("来自数据库验证")
+            if db_type == 'ldap':
+                print("来自ldap验证")
+
+        return wrapper
+
+    return deco
 
 
-@outer
+@auth(db_type='file')
 def index(name):
-    print("欢迎 {} ".format(name))
+    print("index {} ".format(name))
 
 
-index("wq")
+@auth(db_type='mysql')
+def home(name):
+    print("home {} ".format(name))
+
+
+@auth(db_type='ldap')
+def login(name):
+    print("login {} ".format(name))
+
+
+index('wq')
+home('wasdasd')
+login('qweqweq')
+
+
+# 有参装饰器模板
+def deco_para(x, y, z):
+    def outer(func):
+        def wrapper(*args, **kwargs):
+            res = func(*args, **kwargs)
+            return res
+
+        return wrapper
+
+    return outer
+
+
+@deco_para(1, 2, 3)
+def target():
+    pass
