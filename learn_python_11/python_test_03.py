1d22b6319ce19723b1baf2c1441e4358793842e8
Wq <1144502582@qq.com>
2020-10-25 17:22:45 +0800

    作业2

--- learn_python_11/python_test_03.py
+++ learn_python_11/python_test_03.py
@@ -1,28 +1,60 @@
 # 1、编写有参装饰器
-def deco(user, pwd):
+# def deco(user, pwd):
+#     def outer(func):
+#         def wrapper(*args, **kwargs):
+#             print("invoke wrapper!")
+#             input_user = input("input user: ")
+#             input_pwd = input("input user: ")
+#             if input_user == user and input_pwd == pwd:
+#                 res = func(*args, **kwargs)
+#                 return res
+#             else:
+#                 print("error!")
+#
+#         return wrapper
+#
+#     return outer
+#
+#
+# @deco(user="wq", pwd="123")
+# def index(name):
+#     print("hello {} !".format(name))
+#
+#
+# index("wq")
+
+
+# 2、文件开头声明一个字典, 然后再每个函数上加上装饰器注解
+# 完成自动添加进字典的操作
+fun_dic = {}
+
+
+def deco(index):
     def outer(func):
+        fun_dic[index] = func
+
         def wrapper(*args, **kwargs):
-            print("invoke wrapper!")
-            input_user = input("input user: ")
-            input_pwd = input("input user: ")
-            if input_user == user and input_pwd == pwd:
-                res = func(*args, **kwargs)
-                return res
-            else:
-                print("error!")
+            res = func(*args, **kwargs)
+            return res
 
         return wrapper
 
     return outer
 
 
-@deco(user="wq", pwd="123")
+@deco(index="1")
 def index(name):
-    print("hello {} !".format(name))
+    print("index {}".format(name))
 
 
-index("wq")
+@deco(index='2')
+def login():
+    print("login !")
 
 
-# 2、文件开头声明一个字典, 然后再每个函数上加上装饰器注解
-# 完成自动添加进字典的操作
+@deco(index='3')
+def out():
+    print("out !")
+
+
+print(fun_dic)
