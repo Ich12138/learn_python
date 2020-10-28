c21a52a08233001577b27ace3cc0a3b57f9e416a
Wq <1144502582@qq.com>
2020-10-28 17:09:24 +0800

    yield的表达式形式2

--- learn_python_12/python_yield_01.py
+++ learn_python_12/python_yield_01.py
@@ -20,3 +20,25 @@
 #
 # g.close()  # 关闭之后无法传值
 # g.send("asdadadasasd")
+
+
+# yield的表达式形式2
+def dog(name):
+    food_list = []
+    print("道哥 {} 准备吃东西了".format(name))
+    while True:
+        # x拿到的是yield接收到的值, 跟None没有关系
+        x = yield food_list
+        print("道哥{} 吃了 {}".format(name, x))
+        food_list.append(x)
+
+
+g = dog("asdasd")
+res = g.send(None)  # 等同于next(g)
+print(res)
+
+res1 = g.send('一根骨头')
+print(res1)
+
+res2 = g.send('肉包子')
+print(res2)
