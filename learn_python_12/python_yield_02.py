8751c175f2a1dedf3a29158d252e7beb660d4d83
Wq <1144502582@qq.com>
2020-10-29 10:00:56 +0800

    列表生成式-字典生成式

--- learn_python_12/python_yield_02.py
+++ learn_python_12/python_yield_02.py
@@ -3,13 +3,12 @@
     语法格式: 条件成立时要返回的值 if 条件 else 条件不成立返回的值
 """
 
-
-def func(x, y):
-    return x if x > y else y
-
-
-res = func(3, 4)
-print(res)
+# def func(x, y):
+#     return x if x > y else y
+#
+#
+# res = func(3, 4)
+# print(res)
 
 """
 生成式
@@ -29,3 +28,26 @@ l = ['wq_s', 'asd_s', 'asdasd_s', 'zxxc', '456']
 # 方式二
 # new_l = [s for s in l if s.endswith('s')]
 # print(new_l)
+
+"""
+需求:
+    1、把所有小写字母全变成大写
+    2、去掉_s后缀
+"""
+
+# new_l = [s.upper() for s in l]
+# new_l1 = [s.replace('_S', '') for s in new_l]
+#
+# print(new_l)
+# print(new_l1)
+
+# 其他生成式
+# 1、字典生成式
+# keys = ['name', 'age', 'gender']
+# dic = {key: None for key in keys}
+# print(dic)
+
+
+# items = [('name', 'wq'), ('age', 18), ('gender', 'male')]
+# dic = {k: v for k, v in items if k != 'gender'}
+# print(dic)
