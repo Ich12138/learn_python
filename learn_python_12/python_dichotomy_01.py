b406a20a5ea6ec7586ce9b1a458a99205b3592e8
Wq <1144502582@qq.com>
2020-11-01 16:41:46 +0800

    二分法-方法一

--- learn_python_12/python_dichotomy_01.py
+++ learn_python_12/python_dichotomy_01.py
@@ -1,1 +1,23 @@
 # 二分法
+# 需求: 有一个按照从小到大排列的数字列表
+#       根据输入的数字,判断该数字在不在列表内, 在返回下标, 不在返回-1
+num_list = [0, 3, 5, 6, 8, 12, 34, 78, 90]
+
+
+# 方法一:
+# def binary_search(num):
+#     low = 0
+#     high = len(num_list) - 1
+#     while low < high:
+#         mid = (low + high) // 2
+#         if num == num_list[mid]:
+#             return mid
+#         if num > num_list[mid]:
+#             low = mid
+#         if num < num_list[mid]:
+#             high = mid
+#     return -1
+#
+#
+# print(binary_search(6))
+# print(binary_search(100))
