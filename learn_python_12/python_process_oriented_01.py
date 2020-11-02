fd329949b0daf2649dd7ddf92b3f7860d61e4aa8
Wq <1144502582@qq.com>
2020-11-02 19:49:40 +0800

    匿名函数

--- learn_python_12/python_process_oriented_01.py
+++ learn_python_12/python_process_oriented_01.py
@@ -6,3 +6,27 @@
 
 
 # 函数式编程
+# 1、有名函数
+# func = 函数的内存地址
+def func(x, y):
+    return x + y
+
+
+# print(func)  # <function func at 0x0214C028>
+
+# 2、匿名函数: lambda 定义匿名函数
+# 编写格式: lambda 参数1,参数2:表达式
+# print(lambda x, y: x + y)  # <function <lambda> at 0x014F87C0>
+
+# 3、调用匿名函数: 函数内存地址加()
+# 方式一:
+# res = (lambda x, y: x + y)(1, 2)
+# print(res)
+
+# 方式二:
+func1 = lambda x, y: x + y
+print(func1)  # <function <lambda> at 0x01DA87C0>
+res = func1(2, 3)
+print(res)
+
+# 4、匿名函数用于临时调用一次的场景: 更多的是将匿名函数与其他函数配合的场景
