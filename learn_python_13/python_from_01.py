d3fd5308913bec6afa21053b6802997962631cb8
Wq <1144502582@qq.com>
2020-11-11 16:36:35 +0800

    from ... import ... 优缺点

--- learn_python_13/python_from_01.py
+++ learn_python_13/python_from_01.py
@@ -9,6 +9,10 @@
 # 2、运行foo.py将运行过程中产生的名字都丢到模块的名称空间去
 # 3、在当前名称空间拿到一个名字，该名字与模块名称空间中的某一个内存地址
 
-# from foo import x
+# from foo import x x = 模块foo中值的内存地址
 # from foo import change
 # from foo import get
+
+# from ... import ... 导入模块在使用时不用加前缀
+# 优点: 代码更精简
+# 缺点: 容易与当前名称空间混淆
