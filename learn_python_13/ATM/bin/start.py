3b87bc0b7320875f41452bccdb77efa0b17d3eb5
Wq <1144502582@qq.com>
2020-11-17 18:35:34 +0800

    导包的优化方案

--- learn_python_13/ATM/bin/start.py
+++ learn_python_13/ATM/bin/start.py
@@ -2,6 +2,18 @@
 
 # 针对非包, 采用绝对导入
 import sys
-sys.path.append(r'F:\Jetbrains\PythonSoftWare\PythonProjects\learn_python\learn_python_13\ATM')
+# sys.path.append(r'F:\Jetbrains\PythonSoftWare\PythonProjects\learn_python\learn_python_13\ATM')
+# from core import src
+# src.run()
+
+# 导包的优化方案
+import os
+
+# 当前文件的绝对路径
+# print(__file__)
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+sys.path.append(BASE_DIR)
 from core import src
-src.run()
+
+if __name__ == '__main__':
+    src.run()
