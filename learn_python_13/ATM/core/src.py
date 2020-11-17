7e71ea60dd53cbbdbb63399090edd8520710b893
Wq <1144502582@qq.com>
2020-11-17 18:09:11 +0800

    主要逻辑编写

--- learn_python_13/ATM/core/src.py
+++ learn_python_13/ATM/core/src.py
@@ -28,3 +28,16 @@ func_dic = {
     '3': ['提现', withdraw],
     '4': ['转账', transfer],
 }
+
+
+# 项目启动入口
+def run():
+    while True:
+        for k in func_dic:
+            print(func_dic[k][0])
+
+        choice = input("请输入指令编号: ").strip()
+        if choice in func_dic:
+            func_dic[choice][1]()
+        else:
+            print("请重新输入")
