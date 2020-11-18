de86e70a78464e950b26e9f38769b7eb2a66c0b7
Wq <1144502582@qq.com>
2020-11-18 16:10:39 +0800

    优化日志文件路径

--- learn_python_13/ATM/conf/settings.py
+++ learn_python_13/ATM/conf/settings.py
@@ -1,1 +1,2 @@
 # 配置文件
+LOG_PATH = r'F:\Jetbrains\PythonSoftWare\PythonProjects\learn_python\learn_python_13\ATM\log\user.log'
--- learn_python_13/ATM/lib/common.py
+++ learn_python_13/ATM/lib/common.py
@@ -1,9 +1,9 @@
 # 通用公共库函数
 import time
-
+from conf import settings
 
 # 记录日志功能
 def logger(msg):
-    with open(r'F:\Jetbrains\PythonSoftWare\PythonProjects\learn_python\learn_python_13\ATM\log\user.log', mode='at',
+    with open(settings.LOG_PATH, mode='at',
               encoding='utf-8') as log_f:
         log_f.write('%s %s\n' % (time.strftime('%Y-%m-%d %H:%M:%S'), msg))
