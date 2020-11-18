a9a5e988981425a7cde63666d59d77ccfc67f063
Wq <1144502582@qq.com>
2020-11-18 17:01:53 +0800

    时间格式的转换

--- learn_python_14/python_time_01.py
+++ learn_python_14/python_time_01.py
@@ -27,3 +27,31 @@ import datetime
 # print(datetime.datetime.now())  # 2020-11-18 16:31:09.666523
 # print(datetime.datetime.now() + datetime.timedelta(days=-3))
 # print(datetime.datetime.now() + datetime.timedelta(weeks=-3))
+
+# p336 时间模块3
+# 时间模块需要掌握的操作
+# 一: 时间格式的转换
+
+# 结构化时间 -> 时间戳
+# s_time = time.localtime()
+# print(time.mktime(s_time))
+
+# 时间戳 -> 结构化时间
+# tp_time = time.time()
+# print(time.localtime(tp_time))
+
+# print(time.localtime())  # 本地时间    time.struct_time(tm_year=2020, tm_mon=11, tm_mday=18, tm_hour=16, tm_min=53, tm_sec=31, tm_wday=2, tm_yday=323, tm_isdst=0)
+# print(time.gmtime())  # 世界标准时间  time.struct_time(tm_year=2020, tm_mon=11, tm_mday=18, tm_hour=8, tm_min=53, tm_sec=20, tm_wday=2, tm_yday=323, tm_isdst=0)
+
+# print(time.localtime(tp_time))
+# print(time.gmtime(tp_time))
+
+# 结构化时间 -> 格式化时间
+# s_time = time.localtime()
+# res = time.strftime('%Y-%m-%d %H:%M:%S', s_time)
+# print(res)
+
+# 格式化时间 -> 结构化时间
+# format_time = '2035-11-11 16:56:54'
+# res = time.strptime(format_time, '%Y-%m-%d %H:%M:%S')
+# print(res)
