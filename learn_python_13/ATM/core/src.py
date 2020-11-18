e891ee692aef1d919830e7017b050530ba57a80f
Wq <1144502582@qq.com>
2020-11-18 16:02:55 +0800

    转移记录日志功能到common下

--- learn_python_13/ATM/core/src.py
+++ learn_python_13/ATM/core/src.py
@@ -1,12 +1,5 @@
 # 核心代码逻辑
-import time
-
-
-# 记录日志功能
-def logger(msg):
-    with open(r'F:\Jetbrains\PythonSoftWare\PythonProjects\learn_python\learn_python_13\ATM\log\user.log', mode='at',
-              encoding='utf-8') as log_f:
-        log_f.write('%s %s\n' % (time.strftime('%Y-%m-%d %H-%M-%S'), msg))
+from lib.common import logger
 
 
 # 登录功能
