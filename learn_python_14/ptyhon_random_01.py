0245d2e6e88cf6bee2dd3e77caf562c7d0669c67
Wq <1144502582@qq.com>
2020-11-19 11:09:52 +0800

    random模块 基本用法

--- learn_python_14/ptyhon_random_01.py
+++ learn_python_14/ptyhon_random_01.py
@@ -1,1 +1,18 @@
 # p338 random 模块
+import random
+
+# print(random.random())  # 大于0小于1之间的小数
+#
+# print(random.randint(1, 3))  # 1到3闭区间的整数
+#
+# print(random.randrange(1, 3))  # 左闭右开
+#
+# print(random.choice([1, '2', [3, 4]]))  # 随机选择
+#
+# print(random.sample([1, '23', [4, 5]], 2))  # 列表元素两个任意组合
+#
+# print(random.uniform(1, 3))  # 大于1小于三的整数
+
+# item = [1, 3, 4, 5, 6, 7]
+# random.shuffle(item)  # 打乱item的顺序, 相当于 "洗牌"
+# print(item)
