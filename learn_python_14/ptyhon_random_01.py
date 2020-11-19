3f8c944ad7ce6a619cb806815761c218fce1478c
Wq <1144502582@qq.com>
2020-11-19 11:42:54 +0800

    应用: 随机验证码

--- learn_python_14/ptyhon_random_01.py
+++ learn_python_14/ptyhon_random_01.py
@@ -1,6 +1,7 @@
 # p338 random 模块
 import random
 
+
 # print(random.random())  # 大于0小于1之间的小数
 #
 # print(random.randint(1, 3))  # 1到3闭区间的整数
@@ -16,3 +17,17 @@ import random
 # item = [1, 3, 4, 5, 6, 7]
 # random.shuffle(item)  # 打乱item的顺序, 相当于 "洗牌"
 # print(item)
+
+# 应用: 随机验证码
+
+def make_code(size=4):
+    res = ''
+    for i in range(size):
+        char1 = chr(random.randint(65, 90))
+        num1 = str(random.randint(0, 9))
+        res += random.choice([char1, num1])
+    return res
+
+
+res = make_code(6)
+print(res)
