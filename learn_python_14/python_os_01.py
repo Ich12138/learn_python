8b727c0a3a885f5b7a51b6989fce0a54a1a3f5b9
Wq <1144502582@qq.com>
2020-11-22 18:37:20 +0800

    查看文件大小

--- learn_python_14/python_os_01.py
+++ learn_python_14/python_os_01.py
@@ -3,3 +3,7 @@ import os
 # 查询路径下有哪些文件夹
 # res = os.listdir(r'F:\Jetbrains\PythonSoftWare\PythonProjects\learn_python')
 # print(res)
+
+# 查看文件大小
+# size = os.path.getsize(r'F:\Jetbrains\PythonSoftWare\PythonProjects\learn_python\learn_python_14\ptyhon_random_01.py')
+# print(size)
