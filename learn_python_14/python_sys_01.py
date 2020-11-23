e1163bf55949cfcd8b049a138c0d8efdc61b2d6f
Wq <1144502582@qq.com>
2020-11-23 20:02:19 +0800

    打印进度条

--- learn_python_14/python_sys_01.py
+++ learn_python_14/python_sys_01.py
@@ -3,3 +3,23 @@
 #
 # # 获取的是解释器后参数值
 # print(sys.argv)
+
+# p343 打印进度条
+import time
+
+# res = ''
+# for i in range(100):
+#     res += '#'
+#     time.sleep(0.5)
+#     print('\r[%-50s]' % res, end='')
+
+# 模拟文件下载, 打印进度条
+recv_size = 0
+total_size = 3333333
+while recv_size < total_size:
+    time.sleep(0.001)
+    recv_size += 1024
+
+    percent = recv_size / total_size
+    res = int(50 * percent) * '#'
+    print('\r[%-50s] %d%%' % (res, int(percent * 100)), end='')
