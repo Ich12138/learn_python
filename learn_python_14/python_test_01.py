bd47c5bc0e61959104bea131ba31df39855763d1
Wq <1144502582@qq.com>
2020-11-24 14:52:17 +0800

    模拟终端发送命令

---
+++ learn_python_14/python_test_01.py
@@ -0,0 +1,8 @@
+# p345 小测
+# 1、检索文件夹大小的小程序
+import os
+
+# res = os.system('终端内输入的命令')
+res = os.system(
+    'python F:\Jetbrains\PythonSoftWare\PythonProjects\learn_python\learn_python_14\python_run_01.py F:\Jetbrains\PythonSoftWare\PythonProjects\learn_python')
+print(res)
