d0e108374af04fc4866142300d47c8628b206967
Wq <1144502582@qq.com>
2020-11-26 09:42:59 +0800

    来加载特殊格式的配置文件

--- learn_python_15/03_python_configpraser.py
+++ learn_python_15/03_python_configpraser.py
@@ -1,1 +1,19 @@
 # p354 configparser
+# 来加载特殊格式的配置文件
+import configparser
+
+# 1、读取配置文件
+config = configparser.ConfigParser()
+config.read('./doc/test1.ini')
+
+# 2、获取里面所有的sections
+print(config.sections())
+
+# 3、获取某个section下的配置参数
+print(config.options('section2'))
+
+# 4、获取kv键值对
+print(config.items('section2'))
+
+# 5、获取指定的值
+print(config.get('section2', 'k11'))
