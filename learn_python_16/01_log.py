a6797caad053ee91e82abe507fdf07b69dd7daf5
Wq <1144502582@qq.com>
2020-11-28 10:48:24 +0800

    日志配置

--- learn_python_16/01_log.py
+++ learn_python_16/01_log.py
@@ -1,9 +1,31 @@
 # p362 日志级别
 import logging
 
+# 日志配置
+logging.basicConfig(
+    # 1、日志输出位置：1、终端 2、文件
+    # filename='access.log', # 不指定，默认打印到终端
+
+    # 2、日志格式
+    # format='%(asctime)s - %(name)s - %(levelname)s -%(module)s:  %(message)s',
+
+    # 3、时间格式
+    # datefmt='%Y-%m-%d %H:%M:%S %p',
+
+    # 4、日志级别
+    # critical => 50
+    # error => 40
+    # warning => 30
+    # info => 20
+    # debug => 10
+    level=10,
+)
+
 # 输出日志
 logging.debug('调试debug')
 logging.info('消息info')
 logging.warning('警告warn')
 logging.error('错误error')
 logging.critical('严重critical')
+
+# p363 日志格式与输出位置
