3e2bb5521b1148551b1994d70b01a3c0a727fa97
Wq <1144502582@qq.com>
2020-11-28 10:58:29 +0800

    日志配置

--- learn_python_16/01_log.py
+++ learn_python_16/01_log.py
@@ -4,13 +4,13 @@ import logging
 # 日志配置
 logging.basicConfig(
     # 1、日志输出位置：1、终端 2、文件
-    # filename='access.log', # 不指定，默认打印到终端
+    filename='./doc/access.log', # 不指定，默认打印到终端
 
     # 2、日志格式
-    # format='%(asctime)s - %(name)s - %(levelname)s -%(module)s:  %(message)s',
+    format='%(asctime)s - %(name)s - %(levelname)s - %(module)s:  %(message)s',
 
     # 3、时间格式
-    # datefmt='%Y-%m-%d %H:%M:%S %p',
+    datefmt='%Y-%m-%d %H:%M:%S %p',
 
     # 4、日志级别
     # critical => 50
