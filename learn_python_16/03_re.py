1667f4789ca5df9e536767ffa17324bb53601021
Wq <1144502582@qq.com>
2020-11-29 10:39:17 +0800

    重复匹配

--- learn_python_16/03_re.py
+++ learn_python_16/03_re.py
@@ -38,5 +38,41 @@ import re
 # print(res)
 
 # 匹配字符串的末尾
-res = re.findall('xxx$', 'xxxab\fcxxx123\n_*()\t-=xxx')
-print(res)
+# res = re.findall('xxx$', 'xxxab\fcxxx123\n_*()\t-=xxx')
+# print(res)
+
+# 重复匹配：| . | * | ? | .* | .*? | + | {n,m} |
+# .: 匹配任意个字符, 除了\n之外, 指定re.DOTALL才能匹配换行符
+# print(re.findall('a.b', 'a1b'))  # ['a1b']
+
+# *: 左侧字符重复零次或无穷次
+# print(re.findall('ab*', 'a ab abbbb bbbbbbbbbbb'))  # ['a', 'ab', 'abbbb']
+
+# +: 左侧字符重复至少1次或无穷次
+# print(re.findall('ab+', 'a ab abbbb bbbbbbbbbbb'))  # ['ab', 'abbbb']
+
+# ?: 左侧字符重复0次或1次
+# print(re.findall('ab?', 'a ab abbbb bbbbbbbbbbb'))
+
+# {n,m}: 控制左侧字符重复n次到m次
+# {0,} -> *
+# {1,} -> +
+# {0,1} -> ?
+# {n} -> 单独一个n代表左侧字符只出现n次
+
+# print(re.findall('ab{2}', 'abbb'))  # ['abb']
+# print(re.findall('ab{2,4}', 'abbb'))  # ['abb']
+# print(re.findall('ab{1,}', 'abbb'))  # 'ab{1,}' ===> 'ab+'
+# print(re.findall('ab{0,}', 'abbb'))  # 'ab{0,}' ===> 'ab*'
+# print(re.findall('ab{2}c', 'ac abbc abbbbc abbbc'))  # ['abbc']
+
+# 练习: 找出所有的整数和小数
+# print(re.findall('\d+\.?\d*', "asdfasdf123as1.13dfa12adsf1asdf3"))
+
+# [] -> 匹配指定字符中的其中一个
+# 在[]内都是普通字符
+# print(re.findall('a[1*-]b', 'a1b a*b a-b'))  # []内的都为普通字符了，且如果-没有被转意的话，应该放到[]的开头或结尾
+# print(re.findall('a[^1*-]b', 'a1b a*b a-b a=b'))  # []内的^代表的意思是取反，所以结果为['a=b']
+# print(re.findall('a[0-9]b', 'a1b a*b a-b a=b'))  # 0-9
+# print(re.findall('a[a-z]b', 'a1b a*b a-b a=b aeb'))  # 小写
+# print(re.findall('a[a-zA-Z]b', 'a1b a*b a-b a=b aeb aEb'))  # 大小写
