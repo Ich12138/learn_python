ba4c20eeeb8b124e6c64cfcbb7b6eab669a52acb
Wq <1144502582@qq.com>
2020-12-07 10:18:22 +0800

    打印班级和课程信息

--- learn_python_16/07_homework.py
+++ learn_python_16/07_homework.py
@@ -25,17 +25,17 @@ class School:
 
 
 # 1、创建学校
-# lsh_school = School('辽宁石油化工大学', '辽宁省抚顺市望花区辽宁石油化工大学')
-# tute_school = School('天津职业技术师范大学', '天津市津南区大沽南路1310号')
+lsh_school = School('辽宁石油化工大学', '辽宁省抚顺市望花区辽宁石油化工大学')
+tute_school = School('天津职业技术师范大学', '天津市津南区大沽南路1310号')
 
 # 2、为学校开设班级
 # 辽宁石油化工大学 05班、15班
-# lsh_school.related_class('辽宁石油化工大学 05班')
-# lsh_school.related_class('辽宁石油化工大学 15班')
+lsh_school.related_class('辽宁石油化工大学 05班')
+lsh_school.related_class('辽宁石油化工大学 15班')
 
 # 天津职业技术师范大学 1班、2班
-# tute_school.related_class('天津职业技术师范大学 1班')
-# tute_school.related_class('天津职业技术师范大学 2班')
+tute_school.related_class('天津职业技术师范大学 1班')
+tute_school.related_class('天津职业技术师范大学 2班')
 
 # 需求: 查看学校信息和班级信息
 # School.class_info(lsh_school)
@@ -52,9 +52,17 @@ class Class:
     def related_course(self, course_name):
         self.course_name = course_name
 
+    # 打印班级和课程信息
+    def print_course_info(self):
+        print('班级: {}, 课程: {}'.format(self.class_name, self.course_name))
+
 
 lsh_class1 = Class('研21 05班')
 lsh_class2 = Class('研21 15班')
 
 lsh_class1.related_course('python学习')
 lsh_class2.related_course('神经网路与人工智能')
+
+# print(lsh_class1.print_course_info())
+# print(lsh_class2.print_course_info())
+
