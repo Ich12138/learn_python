af82fac7502cf4e0f0c871a857eed56c78069da6
Wq <1144502582@qq.com>
2020-12-08 14:48:10 +0800

    property使用二

--- learn_python_17/01_property.py
+++ learn_python_17/01_property.py
@@ -1,4 +1,4 @@
-# property使用
+# p371 372 property使用
 # 装饰器是在不修改被装饰对象源代码以及调用方式的前提下
 # 为被装饰对象添加新功能的可调用对象
 
@@ -46,3 +46,30 @@
 # print(obj.name)
 # obj.name = 18
 # print(obj.name)
+
+# 三 新用法 效果跟上一个一样
+class People:
+    def __init__(self, name):
+        self.__name = name
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, name):
+        if type(name) is not str:
+            print("必须传入str类型")
+            return
+        self.__name = name
+
+    @name.deleter
+    def name(self):
+        print("不能删除")
+
+
+obj = People('qwe')
+
+print(obj.name)
+obj.name = 18
+print(obj.name)
