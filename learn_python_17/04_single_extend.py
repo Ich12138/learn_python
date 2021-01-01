9e2c1a0d290ac4014254ff7818aa9ac87195ef92
Wq <1144502582@qq.com>
2021-01-01 13:02:02 +0800

    示范一

--- learn_python_17/04_single_extend.py
+++ learn_python_17/04_single_extend.py
@@ -1,1 +1,20 @@
 # p374 单继承背景下的属性查找
+
+# 示范一
+# class Foo:
+#     def f1(self):
+#         print('from Foo.f1')
+#
+#     def f2(self):
+#         print('from Foo.f2')
+#         self.f1()  # 此时self = obj 所以调用的是obj的f1也就是调用的Bar里的f1
+#
+#
+# class Bar(Foo):
+#     def f1(self):
+#         print('from Bar.f1')
+#
+#
+# obj = Bar()
+# obj.f2()
+
