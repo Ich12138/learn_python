e5aecda162e6e938e6ae320e7a39b81db4fabaea
Wq <1144502582@qq.com>
2021-01-01 13:06:09 +0800

    示范三

--- learn_python_17/04_single_extend.py
+++ learn_python_17/04_single_extend.py
@@ -21,19 +21,37 @@
 
 
 # 示范二   子类调用父类的方法
-class Foo:
-    def f1(self):
-        print('from Foo.f1')
+# class Foo:
+#     def f1(self):
+#         print('from Foo.f1')
+#
+#     def f2(self):
+#         print('from Foo.f2')
+#         Foo.f1(self)
+#
+#
+# class Bar(Foo):
+#     def f1(self):
+#         print('from Bar.f1')
+#
+#
+# obj = Bar()
+# obj.f2()
 
-    def f2(self):
-        print('from Foo.f2')
-        Foo.f1(self)
 
-
-class Bar(Foo):
-    def f1(self):
-        print('from Bar.f1')
-
-
-obj = Bar()
-obj.f2()
+# 示范三
+# class Foo:
+#     def __f1(self):
+#         print('from Foo.f1')
+#
+#     def f2(self):
+#         print('from Foo.f2')
+#         self.__f1()  # 调用的是 self._Foo__f1
+#
+# class Bar(Foo):
+#     def f1(self):
+#         print('from Bar.f1')
+#
+#
+# obj = Bar()
+# obj.f2()
